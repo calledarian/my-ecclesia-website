21987ec4015ddf9b6c88b96b5a7d127af66f1bb0
Arian <ariankhadem4@gmail.com>
2025-12-13 13:18:36 +0700

    Delete their imports as well

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
 import Footer from "./components/footer.js";
-import Gallery from "./pages/Gallery";
+// import Gallery from "./pages/Gallery";
 import Contact from "./pages/Contact"; // Ensure the Contact component is imported correctly
 // import Classes from "./pages/Classes";
 import Donation from "./pages/Donation"; // Ensure the Donation component is imported correctly
@@ -10,9 +10,9 @@ import FAQ from "./pages/FAQ.js"; // Ensure the FAQ component is imported correc
 import ScrollToTop from "./components/scrollup"; // Import the ScrollToTop component
 import { Navigate } from "react-router-dom";
 import "./CSS/global.css"; // Import global CSS styles
-import Login from "./components/Login.jsx";
-import PrivateRoute from "./components/PrivateRoute"; // Ensure the PrivateRoute component is imported correctly
-import Dashboard from "./components/Dashboardl"; // Ensure the Dashboard component is imported correctly
+// import Login from "./components/Login.jsx";
+// import PrivateRoute from "./components/PrivateRoute"; // Ensure the PrivateRoute component is imported correctly
+// import Dashboard from "./components/Dashboardl"; // Ensure the Dashboard component is imported correctly
 import "bootstrap-icons/font/bootstrap-icons.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
