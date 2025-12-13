2db4cac73abb9c66d631c791a302a5b2320f5bd6
Arian <ariankhadem4@gmail.com>
2025-12-13 11:56:03 +0700

    Improved Footer

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -26,7 +26,6 @@ const Contact = () => {
       </div>
 
       <div className="map-container">
-        <h3>Location:</h3>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2967.029168647145!2d104.88323247354342!3d11.584849343765347!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x310951118e0c0343%3A0xc035b379dec66b7a!2sCambodia%20Bible%20Education%20Centre%20(BEC)!5e1!3m2!1sen!2skh!4v1743417939523!5m2!1sen!2skh"
           width="400"
@@ -43,7 +42,10 @@ const Contact = () => {
         <h3>Follow us:</h3>
         <ul>
           <li>
-            <a href="https://www.facebook.com/PPbec/">Facebook</a>
+            <a style={{ color: "blue"}} href="https://www.facebook.com/PPbec/" target="_blank" rel="noreferrer">Facebook</a>
+          </li>
+          <li>
+            <a style={{ color: "red"}} href="https://instagram.com/fieldworkers_in_cambodia/" target="_blank" rel="noreferrer">Instagram</a>
           </li>
         </ul>
       </div>
